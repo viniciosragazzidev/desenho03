26df9e091e14ae6eef4f9f467f5566b29705a193
Vinicios Ragazzi <125518719+viniciosragazzidev@users.noreply.github.com>
2023-12-17 16:44:53 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 const textSpan = document.querySelector(".text_SPAN");
 
 const flowers = document.querySelectorAll(".flowers");
-
+const audio = new Audio("./audio.mp3");
 textSpan.addEventListener("click", () => {
   textSpan.classList.add("hidden");
+  audio.play();
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
     clearTimeout(c);
